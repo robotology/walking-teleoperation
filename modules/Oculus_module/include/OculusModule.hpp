a21174000003b3577edd655b39abf4b59a4d16e3
Kourosh Darvish <kourosh.darvish@gmail.com>
2019-08-03 00:28:07 +0200

    add option to cmake and OculusModule Code

--- modules/Oculus_module/include/OculusModule.hpp
+++ modules/Oculus_module/include/OculusModule.hpp
@@ -28,8 +28,10 @@
 #include <HeadRetargeting.hpp>
 #include <TorsoRetargeting.hpp>
 
+#ifdef ENABLE_LOGGER
 #include <matlogger2/matlogger2.h>
 #include <matlogger2/utils/mat_appender.h>
+#endif
 
 /**
  * OculusModule is the main core of the Oculus application. It is goal is to evaluate retrieve the
@@ -136,10 +138,11 @@ private:
                                    only yaw. */
 
     bool m_enableLogger; /**< log the data (if ON) */
+#ifdef ENABLE_LOGGER
     XBot::MatLogger2::Ptr m_logger; /**< */
     XBot::MatAppender::Ptr m_appender;
     std::string m_logger_prefix{"oculus"};
-
+#endif
     /**
      * Configure the Oculus.
      * @param config configuration object
