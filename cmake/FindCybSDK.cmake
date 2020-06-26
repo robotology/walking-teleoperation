c7e8eca514818f9fd0b8bee73d05dd1bc2619a1b
iCubGenova04 <iCubGenova04@example.com>
2020-06-26 14:47:05 +0200

    update the virtualizer module for the new cyberith virtualizer treadmill

--- cmake/FindCybSDK.cmake
+++ cmake/FindCybSDK.cmake
@@ -1,86 +1,67 @@
-# Copyright 2018 Istituto Italiano di Tecnologia (IIT)
-# @author Mohamed Babiker Mohamed Elobaid <mohamed.elobaid@iit.it>
+# Copyright 2020 Istituto Italiano di Tecnologia (IIT)
+# @author Kourosh Darvish <kourosh.darvish@iit.it>
 
-# Finds the Cyb SDK
+# Finds the Sense Glove SDK
 #
 # This will define the following variables::
 #
-#   CybSDK_FOUND    - True if the system has the CybSDK 
-#   CybSDK_VERSION  - The version of the CybSDK  which was found
-#
-#
+#   CybSDK_FOUND         - True if the system has the Sense Glove SDK
+#   CybSDK_INCLUDE_DIR  - The include directory of the SDK
+#   CybSDK_LIBRARIES     - The name of the libraries to link against.
+###############################
 
-##### Utility #####
-
-# Check Directory Macro
-macro(CHECK_DIR _DIR)
-  if(NOT EXISTS "${${_DIR}}")
-    message(WARNING "Directory \"${${_DIR}}\" not found.")
-    set(CybSDK_FOUND FALSE)
-    unset(_DIR)
-  endif()
-endmacro()
-
-# Check Files Macro
-macro(CHECK_FILES _FILES _DIR)
-  set(_MISSING_FILES)
-  foreach(_FILE ${${_FILES}})
-    if(NOT EXISTS "${_FILE}")
-      get_filename_component(_FILE ${_FILE} NAME)
-      set(_MISSING_FILES "${_MISSING_FILES}${_FILE}, ")
-    endif()
-  endforeach()
-  if(_MISSING_FILES)
-    message(WARNING "In directory \"${${_DIR}}\" not found files: ${_MISSING_FILES}")
-    set(CybSDK_FOUND FALSE)
-    unset(_FILES)
-  endif()
-endmacro()
-
-# Target Platform
-set(TARGET_PLATFORM)
-if(NOT CMAKE_CL_64)
-  set(TARGET_PLATFORM x86)
+# Check Directory of the CybSDK_DIR
+if(NOT DEFINED ENV{CybSDK_DIR})
+  message( FATAL_ERROR "Environment variable {CybSDK_DIR} is not defined: $ENV{CybSDK_DIR}" )
 else()
-  set(TARGET_PLATFORM x64)
+  message("Environment variable {CybSDK_DIR}: $ENV{CybSDK_DIR}" )
 endif()
 
+if(NOT DEFINED ENV{CybSDK_INCLUDE_DIRS})
+  message( FATAL_ERROR "Environment variable {CybSDK_INCLUDE_DIRS} is not defined: $ENV{CybSDK_INCLUDE_DIRS}" )
+else()
+  message("Environment variable {CybSDK_INCLUDE_DIRS}: $ENV{CybSDK_INCLUDE_DIRS}" )
+endif()
 ##### Find CybSDK #####
 
-# Found
-set(CybSDK_FOUND TRUE)
-if(MSVC_VERSION LESS 1700)
-  message(WARNING "CybSDK supported Visual Studio 2012 or later.")
-  set(CybSDK_FOUND FALSE)
+find_path(CybSDK_INCLUDE_DIR NAMES CVirt.h CVirtDevice.h HINTS $ENV{CybSDK_INCLUDE_DIRS})
+
+if(WIN32)
+  find_library(CybSDK_LIBRARY NAMES CybSDK HINTS ${CybSDK_DIR})
+else()
+  message(FATAL_ERROR  "The Sense Glove SDK is not supported for this operating system.")
 endif()
 
-# Root Directoty
-set(CybSDK_DIR)
-if(CybSDK_FOUND)
-  set(CybSDK_DIR $ENV{CybSDK_DIR} CACHE PATH "CybSDK Path." FORCE)
-  check_dir(CybSDK_DIR)
+find_package_handle_standard_args(CybSDK_LIBRARY DEFAULT_MSG CybSDK_LIBRARIES
+                                                      CybSDK_INCLUDE_DIR)
+
+
+if(CybSDK_INCLUDE_DIR AND CybSDK_LIBRARY)
+	set(CybSDK_FOUND TRUE)
+else()
+	set(CybSDK_FOUND FALSE)
 endif()
+
+set(CybSDK_LIBRARIES ${CybSDK_LIBRARY})
+
+message(STATUS "CybSDK_FOUND: ${CybSDK_FOUND}")
+message(STATUS "CybSDK_LIBRARIES: ${CybSDK_LIBRARIES}")
+message(STATUS "CybSDK_INCLUDE_DIR: ${CybSDK_INCLUDE_DIR}")
+
+#set(CybSDK_FOUND TRUE)
+#if(NOT CybSDK )
+#  message(FATAL_ERROR "Sense Glove not found.")
+#  set(CybSDK_FOUND FALSE)
+#endif()
 
 # Include Directories
-set(CybSDK_INCLUDE_DIRS)
-if(CybSDK_FOUND)
-  set(CybSDK_INCLUDE_DIRS ${CybSDK_DIR}/include)
-  check_dir(CybSDK_INCLUDE_DIRS)
-endif()
+#set(CybSDK_INCLUDE_DIRS)
+#if(CybSDK_FOUND)
+#  set(CybSDK_INCLUDE_DIRS $ENV{CybSDK_DIR}/Core/SGCoreCpp/incl)
+#endif()
 
-# Library Directories
-set(CybSDK_LIBRARY_DIRS)
-if(CybSDK_FOUND)
-  set(CybSDK_LIBRARY_DIRS ${CybSDK_DIR}/Plugins/${TARGET_PLATFORM})
-  check_dir(CybSDK_LIBRARY_DIRS)
-endif()
+#if(NOT (EXISTS CybSDK_INCLUDE_DIRS))
+#  message(FATAL_ERROR "CybSDK_INCLUDE_DIRS not exists: ${CybSDK_INCLUDE_DIRS}")
+#endif()
 
-# Dependencies
-set(CybSDK_LIBRARIES)
-if(CybSDK_FOUND)
-  set(CybSDK_LIBRARIES ${CybSDK_LIBRARY_DIRS}/CybSDK.dll)
-  check_files(CybSDK_LIBRARIES CybSDK_LIBRARY_DIRS)
-endif()
-
-message(STATUS "CybSDK_FOUND : ${CybSDK_LIBRARIES}")
 
