5a947ef56f236489fcffbbde58a230fbcca86748
Kourosh Darvish <kourosh.darvish@gmail.com>
2020-10-23 12:16:50 +0200

    add logger for the right glove values and forcefeedback values; extend the forcefeedback policy to the all involved axes; add buzz motors feedback to the controller

--- modules/HapticGlove_module/include/HapticGloveModule.hpp
+++ modules/HapticGlove_module/include/HapticGloveModule.hpp
@@ -83,6 +83,7 @@ private:
 
     bool m_enableLogger; /**< log the data (if ON) */
     bool m_useLeftHand, m_useRightHand; /**< use the specided hand if the flag is ON (default value is ON)*/
+    double m_calibrationTimePeriod; /**< calibration time period [sec] */
 #ifdef ENABLE_LOGGER
     XBot::MatLogger2::Ptr m_logger; /**< */
     XBot::MatAppender::Ptr m_appender;
