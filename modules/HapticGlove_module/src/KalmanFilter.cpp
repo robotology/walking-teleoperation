fd4c57947aa81748c7ae065cc68bc1254f0b796c
Kourosh Darvish <kourosh.darvish@gmail.com>
2020-12-13 20:47:05 +0100

    add Kalman Filter class

---
+++ modules/HapticGlove_module/src/KalmanFilter.cpp
@@ -0,0 +1,83 @@
+/**
+ * @file KalmanFilter.cpp
+ * @authors  Kourosh Darvish <kourosh.darvish@iit.it>
+ * @copyright 2020 iCub Facility - Istituto Italiano di Tecnologia
+ *            Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ * @date 2020
+ */
+
+
+
+#include <KalmanFilter.hpp>
+
+
+//Eigen_Mat m_M; /**< E[ (x(t)- x_bar(t))(x(t)- x_bar(t))^T ] : n*n positive matrix */
+//Eigen_Mat m_R; /**< E[ v(t) v(t)^T ] : p*p positive matrix */
+//Eigen_Mat m_P; /**< P_inv= M_inv + H^T R_inv H*/
+//Eigen_Mat m_K; /**< P H^T R_inv */
+
+//Eigen_Mat m_x_bar; /**< state estimation before using the measurements */
+//Eigen_Mat m_x_hat; /**< E[x(t)]  */
+//Eigen_Mat m_w_bar; /**< E[w(t)] */
+
+//size_t m_n; /**< size state vector (x)*/
+//size_t m_p; /**< Size of measure vecotr(z)*/
+//size_t m_m; /**< size of w vector*/
+//double m_dt;/**< sampling time*/
+
+
+KalmanFilter::KalmanFilter(const double dt,
+                           const size_t n,
+                           const size_t p,
+                           const size_t m,
+                           const Eigen::MatrixXd& F,
+                           const Eigen::MatrixXd& G,
+                           const Eigen::MatrixXd& H,
+                           const Eigen::MatrixXd& R,
+                           const Eigen::MatrixXd& Q): m_dt(dt), m_n(n), m_p(p), m_m(m),
+    m_F(F), m_G(G), m_H(H), m_R(R), m_Q(Q)
+{
+
+    m_Phi = Eigen::MatrixXd::Identity(n,n) + m_F * m_dt;
+    m_Gamma = m_G * m_dt;
+    Ht_Rinv = m_H.transpose() * m_R.inverse();
+    Ht_Rinv_H = m_H.transpose() * m_R.inverse() * m_H;
+    Gamma_Q_GammaT = m_Gamma * m_Q * m_Gamma.transpose();
+}
+
+KalmanFilter::~KalmanFilter(){}
+
+bool KalmanFilter::init(const Eigen::MatrixXd& x0, const Eigen::MatrixXd& M0){
+    m_x_bar=x0;
+    m_M= M0;
+
+    return true;
+}
+
+bool KalmanFilter::SetNewMeasure(const Eigen::MatrixXd& z){
+    m_z=z;
+    return true;
+}
+
+bool KalmanFilter::EstimateNextState(Eigen::MatrixXd& x_hat)
+{
+
+    /*
+     * J= 1/2 [(x-x_bar) M^(-1)(x-x_bar) + (z-Hx) R^(-1)(z-Hx)]
+     */
+    m_P =(m_M.inverse() + Ht_Rinv_H).inverse();
+    m_K = m_P * Ht_Rinv;
+    m_x_hat = m_x_bar + m_K * (m_z - m_H * m_x_bar);
+    m_x_bar = m_Phi * m_x_hat + m_Gamma * m_w_bar;
+    m_M = m_Phi * m_P * m_Phi.transpose() + m_Gamma * m_Q * m_Gamma.transpose();
+
+    x_hat= m_x_hat;
+    return true;
+}
+
+bool KalmanFilter::GetInfo( Eigen::MatrixXd& x_hat, Eigen::MatrixXd& P){
+
+    x_hat= m_x_hat;
+    P= m_P;
+    return true;
+}
