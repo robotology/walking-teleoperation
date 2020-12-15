6ae1cc53f60c844118f5ef6fc1a24ddec5a5655b
Kourosh Darvish <kourosh.darvish@gmail.com>
2020-12-15 20:14:45 +0100

    Add motor estimation class on top of Kalman filter

--- modules/HapticGlove_module/include/KalmanFilter.hpp
+++ modules/HapticGlove_module/include/KalmanFilter.hpp
@@ -24,10 +24,16 @@ typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> E
 class KalmanFilter{
 private:
 
+
+
+    size_t m_n; /**< size state vector (x)*/
+    size_t m_m; /**< size of w vector*/
+    size_t m_p; /**< Size of measure vecotr(z)*/
+    double m_dt;/**< sampling time*/
+
     Eigen_Mat m_F; /**< LTI Continuous system dynamics Matrix Dx(t)= Fx(t)+ Gw(t), size: n*n */
     Eigen_Mat m_G; /**< LTI Continuous system input Matrix Dx(t)= Fx(t)+ Gw(t), size:  n*m */
     Eigen_Mat m_H; /**< Measurement Matrix Z(t)= Hx(t)+ v(t), size: p*n */
-
 
     Eigen_Mat m_Phi; /**< LTI Discrete system dynamics Matrix x(i+1)= Phi X(i)+ Gamma w(i), size: n*n */
     Eigen_Mat m_Gamma; /**< LTI Discrete system input Matrix x(i+1)= Phi X(i)+ Gamma w(i), size: n*m */
@@ -35,6 +41,7 @@ private:
     Eigen_Mat m_M; /**< E[ (x(t)- x_bar(t))(x(t)- x_bar(t))^T ], size:  n*n positive matrix */
     Eigen_Mat m_R; /**< E[ v(t) v(t)^T ], size:  p*p positive matrix */
     Eigen_Mat m_Q;  /**< E[ (w(t) -w_bar(t)) (w(t) -w_bar(t))^T ], size:  m*m positive matrix */
+
     Eigen_Mat m_P; /**< P_inv= M_inv + H^T R_inv H, size: n*n */
     Eigen_Mat m_K; /**< P H^T R_inv, size: n*p */
 
@@ -42,12 +49,6 @@ private:
     Eigen_Mat m_x_hat; /**< E[x(t)], size: n*1  */
     Eigen_Mat m_w_bar; /**< E[w(t)], size: m*1 */
     Eigen_Mat m_z; /**< z(t) measurement vector, size: p*1 */
-
-
-    size_t m_n; /**< size state vector (x)*/
-    size_t m_m; /**< size of w vector*/
-    size_t m_p; /**< Size of measure vecotr(z)*/
-    double m_dt;/**< sampling time*/
 
     Eigen_Mat Ht_Rinv;/**< H^T R^(-1), size: n*p */
     Eigen_Mat Ht_Rinv_H;/**< H^T R^(-1), size: n*n*/
@@ -66,13 +67,16 @@ public:
                  const Eigen::MatrixXd& Q);
     ~KalmanFilter();
 
-    bool init(const Eigen::MatrixXd& x0, const Eigen::MatrixXd& M0);
+    bool Initialize(const Eigen::MatrixXd& x0, const Eigen::MatrixXd& M0);
 
     bool  SetNewMeasure(const Eigen::MatrixXd& z);
 
     bool  EstimateNextState(Eigen::MatrixXd& x_hat);
 
-    bool  GetInfo(Eigen::MatrixXd& x_hat, Eigen::MatrixXd& P);
+    bool EstimateNextState(const Eigen::MatrixXd& z, Eigen::MatrixXd& x_hat);
+
+
+    bool  GetInfo(Eigen::VectorXd& x_hat, Eigen::VectorXd& P);
 
 };
 #endif // KALMANFILTER_H
---
+++ modules/HapticGlove_module/src/MotorEstimation.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file MotorEstimation.cpp
+ * @authors  Kourosh Darvish <kourosh.darvish@iit.it>
+ * @copyright 2020 iCub Facility - Istituto Italiano di Tecnologia
+ *            Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ * @date 2020
+ */
+
+
+#include <MotorEstimation.hpp>
+
+MotorEstimation::MotorEstimation(const double dt, const Eigen::MatrixXd& R, const Eigen::MatrixXd& Q){
+
+    m_n = 3;
+    m_m = 3;
+    m_p = 1;
+
+    m_F=Eigen::MatrixXd::Zero(m_n,m_n);
+    m_F(0,1)=1.0;
+    m_F(1,2)=1.0;
+    m_G=Eigen::MatrixXd::Identity(m_n,m_m);
+    m_H=Eigen::MatrixXd::Zero(m_p,m_n);
+    m_H(0,0)=1.0;
+
+
+    m_kf= std::make_unique<KalmanFilter>(dt, m_n, m_p, m_m, m_F, m_G, m_H, R, Q);
+
+}
+MotorEstimation::~MotorEstimation(){
+    m_kf->~KalmanFilter();
+}
+bool MotorEstimation::Initialize(const Eigen::MatrixXd& z0){
+
+    Eigen::MatrixXd x0= m_H * z0;
+    Eigen::MatrixXd  M0=Eigen::MatrixXd::Identity(m_n,m_n);
+
+    return m_kf->Initialize(x0,M0);
+}
+
+bool MotorEstimation::EstimateNextState(const Eigen::MatrixXd& z, Eigen::MatrixXd& x_hat){
+
+    return m_kf->EstimateNextState(z, x_hat);
+}
+
+bool  MotorEstimation::GetInfo(Eigen::VectorXd& x_hat, Eigen::VectorXd& P){
+
+    return m_kf->GetInfo( x_hat, P);
+}
