c420662e942fcf5c4c887889fb018309db1a9a35
Stefano <stefano.dafarra@gmail.com>
2021-01-26 15:24:47 +0100

    Added first version of FaceExpressionsRetargeting module.
    Some initial refactory of the face expression retargeting module.
    
    Added configuration file for the face_expressions_from_mic module.
    
    Added mutex.
    
    Moved the FaceExpressionsRetargeting module to a separate class.

---
+++ modules/FaceExpressions_module/include/FaceExpressionsRetargeting.hpp
@@ -0,0 +1,62 @@
+/**
+ * @file FaceExpressionsRetargeting.hpp
+ * @authors Stefano Dafarra <stefano.dafarra@iit.it>
+ * @copyright 2021 iCub Facility - Istituto Italiano di Tecnologia
+ *            Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ * @date 2021
+ */
+
+#ifndef FACE_EXPRESSIONS_RETARGETING_H
+#define FACE_EXPRESSIONS_RETARGETING_H
+
+#include <fvad.h> /** The external library used to detect the voice. **/
+#include <yarp/sig/Sound.h> /** Needed to receive and use the output of the microphone. **/
+#include <yarp/os/BufferedPort.h> /** Needed to open the input port. **/
+#include <yarp/os/RFModule.h> /** We inherit from this. **/
+#include <yarp/os/RpcClient.h> /** Needed to control the face expressions. **/
+#include <mutex> /** For mutex and lock_guard. **/
+
+/**
+ * @brief The FaceExpressionsRetargetingModule class allows to control the mouth of the robot face expressions if it detects a voice
+ * coming from the microphone.
+ */
+class FaceExpressionsRetargetingModule : public yarp::os::RFModule
+{
+
+    Fvad * m_fvadObject {nullptr}; /** The voice activity detection object. **/
+    yarp::os::BufferedPort<yarp::sig::Sound> m_audioPort; /** The input port for receiving the microphone input. **/
+    std::vector<int16_t> m_copiedSound; /** Internal sound buffer. **/
+    int m_isTalking{0}; /** Status integer to understand if the operator is talking. **/
+    size_t m_switchCounter{0}; /** Internal counter to switch between different mouth positions (to give the impressions it is talking). **/
+    bool m_mouthOpen{false}; /** Internal boolean to check if the robot mouth is open or not. **/
+    size_t m_switchValue{2}; /** Value after switch the value is moved from open to close. **/
+    yarp::os::RpcClient m_emotionsOutputPort; /** The output port to control the face expressions. **/
+    double m_period{0.01}; /** The module period. **/
+    std::string m_state; /** Mouth state. **/
+    std::mutex m_mutex; /** Internal mutex. **/
+
+public:
+
+    /**
+     * Inherited from RFModule.
+     */
+    virtual bool configure(yarp::os::ResourceFinder& rf) override;
+
+    /**
+     * Inherited from RFModule.
+     */
+    virtual double getPeriod() override;
+
+    /**
+     * Inherited from RFModule.
+     */
+    virtual bool updateModule() override;
+
+    /**
+     * Inherited from RFModule.
+     */
+    virtual bool close() override;
+};
+
+
+#endif // FACE_EXPRESSIONS_RETARGETING_H
---
+++ modules/FaceExpressions_module/src/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file main.cpp
+ * @authors Stefano Dafarra <stefano.dafarra@iit.it>
+ * @copyright 2021 iCub Facility - Istituto Italiano di Tecnologia
+ *            Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ * @date 2021
+ */
+
+#include <FaceExpressionsRetargeting.hpp>
+#include <yarp/os/Network.h>
+#include <yarp/os/LogStream.h>
+
+int main(int argc, char* argv[])
+{
+
+    yarp::os::Network yarp;
+    if (!yarp.checkNetwork())
+    {
+        yError()<<"[main] Unable to find YARP network";
+        return EXIT_FAILURE;
+    }
+
+    // prepare and configure the resource finder
+    yarp::os::ResourceFinder& rf = yarp::os::ResourceFinder::getResourceFinderSingleton();
+
+    rf.setDefaultConfigFile("faceExpressionsConfig.ini");
+
+    rf.configure(argc, argv);
+
+    FaceExpressionsRetargetingModule speechmodule;
+
+    return speechmodule.runModule(rf);
+}
