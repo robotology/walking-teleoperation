9c625d380f613a3cb6d658ab482aed73654d1e79
Kourosh <kourosh.darvish@gmail.com>
2021-02-18 16:58:55 +0100

    HapticGlove: add a linear regression class to the project; clean up more the code related to learning the coupling

---
+++ modules/HapticGlove_module/src/LinearRegression.cpp
@@ -0,0 +1,60 @@
+#include <LinearRegression.hpp>
+
+
+LinearRegression::LinearRegression(){
+
+}
+
+LinearRegression::~LinearRegression(){
+
+}
+
+bool LinearRegression::Initialize(){
+
+    return true;
+}
+
+bool LinearRegression::LearnOneShot(const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>&  inputData,
+                                    const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>&  ouputData,
+                                    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& tetha){
+
+    // TODEL
+    std::cout << "[LinearRegression::LearnOneShot()]"<<std::endl;
+
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> xT_x
+        = (inputData.transpose() * inputData);
+    std::cout << "xT_x: " << xT_x.rows() << xT_x.cols()<<std::endl;
+    std::cout  << "xT_x.determinant(): " << xT_x.determinant()<<std::endl;
+    Eigen::FullPivLU<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> lu(
+        xT_x);
+    std::cout << "lu.rank(): " << lu.rank()<<std::endl;
+    // TODEL
+
+
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> coeff
+        = ((inputData.transpose() * inputData).inverse()) * inputData.transpose(); // m X o
+
+        tetha = coeff * ouputData; // m X 1
+
+    return true;
+}
+
+bool LinearRegression::LearnOneShotMatrix(const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>&  inputData,
+                                    const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>&  ouputData,
+                                    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& tetha){
+
+    for (size_t i = 0; i < ouputData.cols(); i++)
+    {
+        Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> tetha_i;
+        LearnOneShot(inputData, ouputData.col(i), tetha_i);
+        push_back_row(tetha, tetha_i.transpose());
+    }
+
+    return true;
+}
+
+bool LinearRegression::LearnIncrementally(){
+
+    return true;
+}
+
