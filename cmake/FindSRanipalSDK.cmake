827090a7af6fd7007ae01652e7306b5cdfe37bfc
icub-console-cuda <icub@foo.com>
2021-06-09 11:50:33 +0200

    Avoid adding the dll when finding SRanipal

--- cmake/FindSRanipalSDK.cmake
+++ cmake/FindSRanipalSDK.cmake
@@ -3,12 +3,11 @@
 set(SRanipal_SDK_DIR $ENV{SRanipal_SDK_DIR} CACHE PATH "Path to the SRanipal C SDK (i.e. the 01_C folder).")
 
 find_path(SRanipalSDK_INCLUDE_DIR SRanipal.h HINTS ${SRanipal_SDK_DIR}/include)
-find_file(SRanipalSDK_LIBRARY NAMES SRanipal.dll HINTS ${SRanipal_SDK_DIR}/bin)
-find_library(SRanipalSDK_IMPLIB NAMES SRanipal HINTS ${SRanipal_SDK_DIR}/lib)
+find_library(SRanipalSDK_LIBRARY NAMES SRanipal HINTS ${SRanipal_SDK_DIR}/lib)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SRanipalSDK DEFAULT_MSG
-  SRanipalSDK_LIBRARY SRanipalSDK_IMPLIB SRanipalSDK_INCLUDE_DIR)
+  SRanipalSDK_LIBRARY SRanipalSDK_INCLUDE_DIR)
 
 if(SRanipalSDK_FOUND AND NOT TARGET SRanipalSDK::SRanipalSDK)
   set(SRanipalSDK_LIBRARIES ${SRanipalSDK_LIBRARY})
@@ -17,8 +16,7 @@ if(SRanipalSDK_FOUND AND NOT TARGET SRanipalSDK::SRanipalSDK)
   add_library(SRanipalSDK::SRanipalSDK UNKNOWN IMPORTED)
   set_target_properties(SRanipalSDK::SRanipalSDK PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES ${SRanipalSDK_INCLUDE_DIR}
-    IMPORTED_LOCATION ${SRanipalSDK_LIBRARY}
-    IMPORTED_IMPLIB ${SRanipalSDK_IMPLIB})
+    IMPORTED_LOCATION ${SRanipalSDK_LIBRARY})
 
-    mark_as_advanced(SRanipalSDK_INCLUDE_DIR SRanipalSDK_LIBRARY SRanipalSDK_IMPLIB)
+    mark_as_advanced(SRanipalSDK_INCLUDE_DIR SRanipalSDK_LIBRARY)
 endif()
